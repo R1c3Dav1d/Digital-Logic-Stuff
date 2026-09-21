dcea89f2dba31d2a5724f62486b66c8f2c4e2561
RiceDavid <69484241+R1c3Dav1d@users.noreply.github.com>
2026-09-20 21:08:25 -0500

    Add testbench for 2-to-1 multiplexer
    This testbench simulates a 2-to-1 multiplexer (mux21) and displays the inputs and output during the simulation.

---
+++ lab 2/tb_mux21.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+/* After running the simulation, if you do not see "Testbench
+ Complete" on the console, then increase simulation time to
+ Say 10000 ns */
+module tb_mux21();
+ reg S, A, B;
+ wire F;
+ reg [3:0] ctr;
+ mux21 uut(S, A, B, F);
+ // The initial block runs procedural code before anything else runs.
+ initial begin
+ $display("Testbench start");
+ $display("S A B | F");
+   {S, A, B, ctr} = 7'b0000000;
+ end
+ // The always block executes procedural (line-by-line) code.
+ always begin
+ #10;
+ $display("%d %d %d | %d", S, A, B, F);
+
+ case (ctr)
+ 0: {S, A, B, ctr} = 7'b0100001;
+ 1: {S, A, B, ctr} = 7'b0010010;
+ 2: {S, A, B, ctr} = 7'b0100011;
+ 3: {S, A, B, ctr} = 7'b0010100;
+ 4: {S, A, B, ctr} = 7'b0100101;
+ 5: {S, A, B, ctr} = 7'b0010110;
+ 6: {S, A, B, ctr} = 7'b0110111;
+ 7: {S, A, B, ctr} = 7'b1111000;
+ 8: {S, A, B, ctr} = 7'b1101001;
+ 9: {S, A, B, ctr} = 7'b1011010;
+ 10: {S, A, B, ctr} = 7'b1101011;
+ 11: {S, A, B, ctr} = 7'b1011100;
+ 12: {S, A, B, ctr} = 7'b1101101;
+ 13: {S, A, B, ctr} = 7'b1011110;
+ 14: begin
+ $display("Testbench end");
+ $finish;
+ end
+ endcase
+ end
+endmodule
